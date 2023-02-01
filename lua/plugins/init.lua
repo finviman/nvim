1dfccaa288acf0dc0f6fee77362e224d6ff651fc
acerphoenix <li.acerphoenix@gmail.com>
2023-02-01 19:52:59 +0800

    ha

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,82 +1,45 @@
--- lazy load gitsign
-local function config_gitSign()
-require('gitsigns').setup{
-  signs = {
-    add          = {hl = 'GitSignsAdd'   , text = '▌', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-    change       = {hl = 'GitSignsChange', text = '▌', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-    delete       = {hl = 'GitSignsDelete', text = '◺', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    topdelete    = {hl = 'GitSignsDelete', text = '◺', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    changedelete = {hl = 'GitSignsChange', text = '▌', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-  },
-  current_line_blame_opts = {
-    virt_text = true,
-    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-    delay = 300,
-  }
-}
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
--- lazy load telescope
-local function config_tele()
-    require('telescope').load_extension('fzf')
-    local action_layout = require('telescope.actions.layout')
-    local actions = require('telescope.actions')
-    require('telescope').setup{
-    defaults = {
-        prompt_prefix = "✎ ",
-        selection_caret = "➳ ",
-        layout_strategy = "horizontal",
-        layout_config = {
-            horizontal ={
-                preview_width = 80
-            }
-        },
-        mappings = {
-          i = {
-            ["<esc>"] = actions.close,
-            ["<C-k>"] = action_layout.toggle_preview
-          }
-        },
-      path_display = {
-        "shorten",
-        "absolute",
-        },
+require("lazy").setup(
+	'glepnir/zephyr-nvim',
+	'nvim-lualine/lualine.nvim',
+	'kyazdani42/nvim-web-devicons',
+	'nvim-lua/plenary.nvim',
+plugins, 
+opts
+)
 
-        dynamic_preview_title=true
-    },
-    extensions = {
-    fzf = {
-      override_generic_sorter = false, -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case" the default case_mode is "smart_case"
-    }
-  }
-}
-end
 return require('packer').startup({function(use)
-	use {'wbthomason/packer.nvim'}
-	use {'glepnir/zephyr-nvim'}
-	use {'nvim-lualine/lualine.nvim'}
-	use {'kyazdani42/nvim-web-devicons'}
-	use {'nvim-lua/plenary.nvim'}
+	use {'wbthomason/packer.nvim',
 	use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate',
     config = function()
       require'nvim-treesitter.configs'.setup {
       highlight = {
         enable = true,              -- false will disable the whole extension
-      },
-    }
+      ,,
+    ,
   end
-  }
+  ,
   use({
     "glepnir/lspsaga.nvim",
     branch = "main",
     config = function()
-        require("lspsaga").setup({})
+        require("lspsaga").setup({,)
     end,
-    requires = { {"nvim-tree/nvim-web-devicons"} }
-  })
-  use {'onsails/lspkind.nvim'}
+    requires = { {"nvim-tree/nvim-web-devicons", ,
+  ,)
+  use {'onsails/lspkind.nvim',
   use {'hrsh7th/nvim-cmp',
     requires = {
         "hrsh7th/cmp-buffer",
@@ -84,68 +47,68 @@ return require('packer').startup({function(use)
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-vsnip",
         "hrsh7th/vim-vsnip",
-    }
-  }
-	use {'nvim-telescope/telescope.nvim',opt=true,cmd={'Telescope'},config=config_tele,requires = { {'nvim-lua/plenary.nvim'} }}
-	use {'nvim-telescope/telescope-fzf-native.nvim',run = 'make'}
+    ,
+  ,
+	use {'nvim-telescope/telescope.nvim',opt=true,cmd={'Telescope',,config=config_tele,requires = { {'nvim-lua/plenary.nvim', ,,
+	use {'nvim-telescope/telescope-fzf-native.nvim',run = 'make',
 	use {'kyazdani42/nvim-tree.lua',
     config = function() 
     require'nvim-tree'.setup {
         respect_buf_cwd= true,
         filters = {
-            custom = {'.git', 'node_modules', '.cache','.idea','.settings','.classpath','.project','*.iml','target'}
-        },
+            custom = {'.git', 'node_modules', '.cache','.idea','.settings','.classpath','.project','*.iml','target',
+        ,,
         view = {
             mappings = {
                 list = {
-                    {key="C",action="cd"},
-                    {key="O",action="expand_all"},
-                    {key="o",action="preview"},
-                }
-            }
-        },
+                    {key="C",action="cd",,
+                    {key="O",action="expand_all",,
+                    {key="o",action="preview",,
+                ,
+            ,
+        ,,
         on_attach = "disabled",
-        remove_keymaps = {"<C-e>"}
-    } end,
-    opt=true,cmd={'NvimTreeFindFile','NvimTreeToggle'}}
-	use {'neovim/nvim-lspconfig'}
-  use {'williamboman/nvim-lsp-installer'}
-	use {'folke/which-key.nvim'}
-	use {'laishulu/vim-macos-ime'}
-	use {'lewis6991/gitsigns.nvim',opt=true,event='BufRead',config=config_gitSign}
-	use {'sindrets/diffview.nvim',opt=true,cmd='DiffviewOpen'}
+        remove_keymaps = {"<C-e>",
+    , end,
+    opt=true,cmd={'NvimTreeFindFile','NvimTreeToggle',,
+	use {'neovim/nvim-lspconfig',
+  use {'williamboman/nvim-lsp-installer',
+	use {'folke/which-key.nvim',
+	use {'laishulu/vim-macos-ime',
+	use {'lewis6991/gitsigns.nvim',opt=true,event='BufRead',config=config_gitSign,
+	use {'sindrets/diffview.nvim',opt=true,cmd='DiffviewOpen',
 	use {'simrat39/symbols-outline.nvim',opt=true,cmd='SymbolsOutline',
     config = function()
       require("symbols-outline").setup()
     end
-  }
+  ,
 	use {'akinsho/nvim-toggleterm.lua',
     config = function()
-      require("toggleterm").setup{}
+      require("toggleterm").setup{,
     end
-  }
-  use {'steelsojka/pears.nvim',config=function() require "pears".setup() end}
-  use {'lewis6991/impatient.nvim'}
-  use {'phaazon/hop.nvim', as = 'hop',opt=true,cmd={'HopChar2','HopChar2MW'},
+  ,
+  use {'steelsojka/pears.nvim',config=function() require "pears".setup() end,
+  use {'lewis6991/impatient.nvim',
+  use {'phaazon/hop.nvim', as = 'hop',opt=true,cmd={'HopChar2','HopChar2MW',,
     config = function()
-    require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+    require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' ,
     end
-  }
+  ,
   use{'winston0410/commented.nvim',
     config = function() require('commented').setup({
-          keybindings = {n = "gc", v = "gc", nl = "gcc"},
-    }) end
-  }
+          keybindings = {n = "gc", v = "gc", nl = "gcc",,
+    ,) end
+  ,
   use {"ahmedkhalf/project.nvim",
     config = function()
-        require("project_nvim").setup{detection_methods = { "pattern" }}
+        require("project_nvim").setup{detection_methods = { "pattern" ,,
     end
-  }
-  use {"mattn/emmet-vim",opt=true,ft={'vue','html','css','scss','sass'}
-  }
+  ,
+  use {"mattn/emmet-vim",opt=true,ft={'vue','html','css','scss','sass',
+  ,
 end,
 config = {
     compile_path = vim.fn.stdpath('config')..'/lua/packer_compiled.lua'
-  }
-})
+  ,
+,)
 
