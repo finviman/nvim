2eb0674d366a94e3fc26c5b88b8c58eb600fb054
acerphoenix <li.acerphoenix@gmail.com>
2023-02-03 10:52:51 +0800

    start performance

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -129,7 +129,7 @@ return {
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-vsnip",
-        "hrsh7th/vim-vsnip",
+        --  "hrsh7th/vim-vsnip",
         "onsails/lspkind.nvim",
     },
     config = function()
