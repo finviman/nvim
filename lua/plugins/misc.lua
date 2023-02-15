4b4973267087a1950486409cf7e8c9a66eb4c40c
acerphoenix <li.acerphoenix@gmail.com>
2023-02-15 13:11:58 +0800

    surround

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -50,13 +50,13 @@ return {
     'echasnovski/mini.surround', version = '*',
     opts = {
       mappings = {
-        add = 'Sa', -- Add surrounding in Normal and Visual modes
-        delete = 'Sd', -- Delete surrounding
-        find = 'Sf', -- Find surrounding (to the right)
-        find_left = 'SF', -- Find surrounding (to the left)
-        highlight = 'Sh', -- Highlight surrounding
-        replace = 'Sr', -- Replace surrounding
-        update_n_lines = 'Sn', -- Update `n_lines`
+        add = 'gsa', -- Add surrounding in Normal and Visual modes
+        delete = 'gsd', -- Delete surrounding
+        find = 'gsf', -- Find surrounding (to the right)
+        find_left = 'gsF', -- Find surrounding (to the left)
+        highlight = 'gsh', -- Highlight surrounding
+        replace = 'gsr', -- Replace surrounding
+        update_n_lines = 'gsn', -- Update `n_lines`
         suffix_last = 'l', -- Suffix to search with "prev" method
         suffix_next = 'n', -- Suffix to search with "next" method
       },
